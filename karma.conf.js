0fa80f603c6fb8319e0f7cd857a0c5d82b029d12
fdubois <dubois.frederic@gmail.com>
2014-04-12 21:38:21 +0200

    Finished coverage configuration

--- karma.conf.js
+++ karma.conf.js
@@ -48,7 +48,7 @@ module.exports = function (config) {
 
         // test results reporter to use
         // possible values: 'dots', 'progress', 'junit', 'growl', 'coverage'
-        'reporters': ['progress'],
+        'reporters': ['progress', 'coverage'],
 
         // web server port
         'port': 9876,
